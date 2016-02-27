8612d69bf5f40190691cec2a8c862212ef193523
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-02-27 14:46:07 -0800

    Refactor to compute from packed points array

--- index.js
+++ index.js
@@ -1,67 +1,104 @@
 var util = require('polyline-miter-util')
 
-var lineA = [0, 0]
-var lineB = [0, 0]
-var tangent = [0, 0]
-var miter = [0, 0]
+module.exports = function createBuilder (size) {
+  var cur = [0, 0]
+  var last = [0, 0]
+  var next = [0, 0]
 
-module.exports = function(points, closed) {
-    var curNormal = null
-    var out = []
-    if (closed) {
-        points = points.slice()
-        points.push(points[0])
+  var curNormal = [0, 0]
+  var lineA = [0, 0]
+  var lineB = [0, 0]
+  var tangent = [0, 0]
+  var miter = [0, 0]
+
+  var normals = new Float32Array(size * 2)
+  var miters = new Float32Array(size)
+  var normalIndex, miterIndex;
+
+  function reset () {
+    normalIndex = 0
+    miterIndex = 0
+  }
+
+  function addNext(normal, length) {
+    normals[normalIndex++] = normal[0]
+    normals[normalIndex++] = normal[1]
+    miters[miterIndex++] = length
+  }
+
+  function computeSegment (points, i, total) {
+    var hasNext = i < total - 1;
+    var aix = i * 2
+    var aiy = aix + 1
+    var bix = (i - 1) * 2
+    var biy = bix + 1
+
+    cur[0] = points[aix]
+    cur[1] = points[aiy]
+    last[0] = points[bix]
+    last[1] = points[biy]
+
+    if (hasNext) {
+      cix = (i + 1) * 2
+      ciy = cix + 1
+      next[0] = points[cix]
+      next[1] = points[ciy]
     }
 
-    var total = points.length
-    for (var i=1; i<total; i++) {
-        var last = points[i-1]
-        var cur = points[i]
-        var next = i<points.length-1 ? points[i+1] : null
+    util.direction(lineA, cur, last)
+    util.normal(curNormal, lineA)
 
-        util.direction(lineA, cur, last)
-        if (!curNormal)  {
-            curNormal = [0, 0]
-            util.normal(curNormal, lineA)
-        }
+    if (i === 1) //add initial normals
+      addNext(curNormal, 1)
 
-        if (i === 1) //add initial normals
-            addNext(out, curNormal, 1)
+    if (!next) { //no miter, simple segment
+      util.normal(curNormal, lineA) //reset normal
+      addNext(curNormal, 1)
+    } else { //miter with last
+      //get unit dir of next line
+      util.direction(lineB, next, cur)
 
-        if (!next) { //no miter, simple segment
-            util.normal(curNormal, lineA) //reset normal
-            addNext(out, curNormal, 1)
-        } else { //miter with last
-            //get unit dir of next line
-            util.direction(lineB, next, cur)
+      //stores tangent & miter
+      var miterLen = util.computeMiter(tangent, miter, lineA, lineB, 1)
+      addNext(miter, miterLen)
+    }
+  }
 
-            //stores tangent & miter
-            var miterLen = util.computeMiter(tangent, miter, lineA, lineB, 1)
-            addNext(out, miter, miterLen)
-        }
+  function update (points, closed) {
+    reset()
+
+    if (closed) {
+      // points = points.slice()
+      // points.push(points[0])
+    }
+
+    var total = points.length / 2
+    for (var i = 1; i < total; i++) {
+      computeSegment(points, i, total)
     }
 
     //if the polyline is a closed loop, clean up the last normal
     if (points.length > 2 && closed) {
-        var last2 = points[total-2]
-        var cur2 = points[0]
-        var next2 = points[1]
+      // var last2 = points[total-2]
+      // var cur2 = points[0]
+      // var next2 = points[1]
 
-        util.direction(lineA, cur2, last2)
-        util.direction(lineB, next2, cur2)
-        util.normal(curNormal, lineA)
-        
-        var miterLen2 = util.computeMiter(tangent, miter, lineA, lineB, 1)
-        out[0][0] = miter.slice()
-        out[total-1][0] = miter.slice()
-        out[0][1] = miterLen2
-        out[total-1][1] = miterLen2
-        out.pop()
+      // util.direction(lineA, cur2, last2)
+      // util.direction(lineB, next2, cur2)
+      // util.normal(curNormal, lineA)
+
+      // var miterLen2 = util.computeMiter(tangent, miter, lineA, lineB, 1)
+      // out[0][0] = miter.slice()
+      // out[total-1][0] = miter.slice()
+      // out[0][1] = miterLen2
+      // out[total-1][1] = miterLen2
+      // out.pop()
     }
+  }
 
-    return out
-}
-
-function addNext(out, normal, length) {
-    out.push([[normal[0], normal[1]], length])
+  return {
+    normals,
+    miters,
+    update
+  }
 }
--- test-2d.js
+++ test-2d.js
@@ -1,10 +1,10 @@
 require('canvas-testbed')(render, { once: true })
 
 var vec = require('gl-vec2')
-
-var getNormals = require('./')
 var arc = require('arc-to')
 var curve = require('adaptive-bezier-curve')
+var pack = require('array-pack-2d')
+var createNormalizer = require('./')
 
 var paths = []
 paths.push( [ [40, 40], [80, 30], [80, 60], [125, 33], [115, 100], [50, 120], [70, 150] ] )
@@ -15,7 +15,7 @@ paths.push( { path: [ [50, 50], [100, 50], [100, 100], [50, 100] ], closed: true
 paths.push( [ [30, -60], [80, 10] ] )
 
 function circle(x, y, radius) {
-    //in this case arc-to closes itself by making the 
+    //in this case arc-to closes itself by making the
     //last point equal to the first. we want to fix this
     //to pass in a more typical polyline and get the right normals
     var c = arc(x, y, radius, 0, Math.PI*2, false)
@@ -31,56 +31,68 @@ function render(ctx, width, height) {
     paths.forEach(function(data, i) {
         var path = Array.isArray(data) ? data : data.path
         var closed = typeof data === 'object' && data.closed
+        var packedPath = pack(path)
 
         var cols = 3
         var x = i % cols,
             y = ~~(i / cols)
         ctx.translate(x * 50, y * 50)
-        draw(ctx, path, closed)
+        draw(ctx, packedPath, closed)
     })
     ctx.restore()
 }
 
 function draw(ctx, path, closed) {
-    var thick = 25,
-        halfThick = thick / 2
+    var thick = 25
+    var halfThick = thick / 2
     var psize = 4
-    var tmp = [0,0]
+
+    var pos = [0, 0]
+    var norm = [0, 0]
+    var tmp = [0, 0]
+    var len = 0
+    var ix, iy;
 
     var top = []
     var bot = []
 
     //get the normals of the path
-    var normals = getNormals(path, closed)
-        
+    var normalizer = createNormalizer(path.length / 2)
+    var normals = normalizer.normals
+    var miters = normalizer.miters
+    normalizer.update(path, closed)
+
     //for drawing the join, we can just add the first point
-    if (closed) {
-        path.push(path[0])
-        normals.push(normals[0])
-    }
+    // if (closed) {
+    //     path.push(path[0])
+    //     normals.push(normals[0])
+    // }
 
     //draw our expanded vertices for each point in the path
     ctx.globalAlpha = 0.15
-    path.forEach(function(p, i) {
-        var attrib = normals[i]
-        var norm = attrib[0]
-        var len = attrib[1]
+    for (var i = 0, il = path.length / 2; i < il; i++) {
+        ix = i * 2
+        iy = ix + 1
+
+        vec.set(pos, path[ix], path[iy])
+        vec.set(norm, normals[ix], normals[iy])
+        len = miters[i]
 
         ctx.fillStyle = 'black'
-        ctx.fillRect(p[0]-psize/2, p[1]-psize/2, psize, psize)
+        ctx.fillRect(pos[0] - psize / 2, pos[1] - psize / 2, psize, psize)
 
         ctx.beginPath()
-        vec.scaleAndAdd(tmp, p, norm, len*halfThick)
-        ctx.moveTo(p[0], p[1])
+        vec.scaleAndAdd(tmp, pos, norm, len * halfThick)
+        ctx.moveTo(pos[0], pos[1])
         ctx.lineTo(tmp[0], tmp[1])
         top.push(tmp.slice())
 
-        vec.scaleAndAdd(tmp, p, norm, -len*halfThick)
-        ctx.moveTo(p[0], p[1])
+        vec.scaleAndAdd(tmp, pos, norm, -len * halfThick)
+        ctx.moveTo(pos[0], pos[1])
         ctx.lineTo(tmp[0], tmp[1])
         ctx.stroke()
         bot.push(tmp.slice())
-    })
+    }
 
     //edges
     ctx.globalAlpha = 1
